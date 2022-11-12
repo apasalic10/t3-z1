4d4ac5f91cb2a4be9d4b0e8787878145be34401e
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-11-12 19:25:26 +0100

    add classes MedunarodniBroj and MobilniBroj

---
+++ src/main/java/ba/unsa/etf/rpr/MedunarodniBroj.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class MedunarodniBroj extends TelefonkiBroj{
+
+    private String drzava;
+    private String broj;
+
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
+    public MedunarodniBroj() {}
+
+    @Override
+    public String ispisi() {
+        return drzava + "/" + broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(drzava,broj);
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/MobilniBroj.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class MobilniBroj extends TelefonkiBroj{
+
+    private int mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
+    public MobilniBroj() {}
+
+    @Override
+    public String ispisi() {
+        return  "0" + mobilnaMreza + "/" + broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mobilnaMreza,broj);
+    }
+
+    public int getMobilnaMreza() {
+        return mobilnaMreza;
+    }
+
+    public void setMobilnaMreza(int mobilnaMreza) {
+        this.mobilnaMreza = mobilnaMreza;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+}
